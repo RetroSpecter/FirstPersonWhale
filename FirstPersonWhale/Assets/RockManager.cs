cabafe0a735042c24f17e03ffa528607a6199b8f
Jose Pacio <pacioj@attu.cs.washington.edu>
2019-02-14 16:43:06 -0800

    j u i c e

---
+++ FirstPersonWhale/Assets/RockManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockManager : MonoBehaviour {
+
+    public GameObject[] rocks;
+
+    private void Start()
+    {
+        foreach (GameObject r in rocks)
+            r.SetActive(false);
+    }
+
+    public void ActivateRock(int i) {
+        rocks[i].SetActive(true);
+    }
+}
