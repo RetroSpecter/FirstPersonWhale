9552029091dad7969990d151627c3118e3ce2b79
Jose Pacio <pacioj@uw.edu>
2019-02-24 18:16:49 -0800

    workin on bubbles and added a fader to godrays

---
+++ FirstPersonWhale/Assets/HidingBubbleTrail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingBubbleTrail : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
