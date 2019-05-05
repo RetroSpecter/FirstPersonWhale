cd4cca193540d709a75b0dc57fb97d98418ffd82
Jose Pacio <pacioj@uw.edu>
2019-05-05 10:53:33 -0700

    working on stuff

---
+++ FirstPersonWhale/Assets/HeadAimController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class HeadAimController : MonoBehaviour
+{
+
+    public GameObject headAim;
+    public float turnSpeed = 5;
+    public GameObject[] targets;
+    public int lookTarget;
+    private Vector3 initialPosition;
+    public Rig headRig;
+    [Range(0, 1)]
+    public float trackingWeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = headAim.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lookTarget > -1) {
+            headAim.transform.position = Vector3.Lerp(headAim.transform.position,initialPosition,Time.deltaTime * turnSpeed);
+        } else {
+            headAim.transform.position = Vector3.Lerp(headAim.transform.position, targets[lookTarget].transform.position, Time.deltaTime * turnSpeed);
+        }
+        headRig.weight = Mathf.Lerp(headRig.weight, trackingWeight, Time.deltaTime);
+    }
+
+    public void lookAtTarget(int target) {
+        lookTarget = target;
+    }
+
+    public void resetTarget() {
+        lookTarget = -1;
+    }
+
+    public void setWeight(float targetWeight) {
+        trackingWeight = targetWeight;
+    }
+}
---
+++ FirstPersonWhale/Assets/SetHeadTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetHeadTarget : StateMachineBehaviour
+{
+
+    public int targetIndex;
+    public float weight = 1;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        HeadAimController hac = animator.GetComponent<HeadAimController>();
+        if (hac != null) {
+            hac.setWeight(weight);
+            if (targetIndex == -1)
+                hac.lookAtTarget(targetIndex);
+            else
+                hac.resetTarget();
+        }
+    }
+}
