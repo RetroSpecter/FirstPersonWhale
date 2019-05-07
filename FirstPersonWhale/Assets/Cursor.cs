1d996ffbe5f1e8b8c3f35c244932d3fbb91a765a
Jose Pacio <pacioj@uw.edu>
2019-05-06 23:05:11 -0700

    some light work

--- FirstPersonWhale/Assets/Cursor.cs
+++ FirstPersonWhale/Assets/Cursor.cs
@@ -7,12 +7,12 @@ public class Cursor : MonoBehaviour
 
     public GameObject camera;
     public float speed = 4;
-
+    public float distance = 2;
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, camera.transform.position + camera.transform.forward * 0.5f, Time.deltaTime * speed);
+        transform.position = Vector3.Lerp(transform.position, camera.transform.position + camera.transform.forward * distance, Time.deltaTime * speed);
     }
 }
