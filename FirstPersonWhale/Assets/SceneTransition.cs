72ebf6523e3ef4e027071b309737ef9e176b4b6f
Jose Pacio <pacioj@uw.edu>
2019-05-20 18:40:02 -0700

    transitions to and from menu

---
+++ FirstPersonWhale/Assets/SceneTransition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    //public string levelName = "Scarlet";
+
+    public void transitionTo(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
