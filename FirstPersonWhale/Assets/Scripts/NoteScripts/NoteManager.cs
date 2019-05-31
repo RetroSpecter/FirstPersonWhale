97a281b0c104e9154f858b051a0696222326fd54
Jose Pacio <pacioj@uw.edu>
2019-05-30 18:09:59 -0700

    microphone

---
+++ FirstPersonWhale/Assets/Scripts/NoteScripts/NoteManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public NoteBubble[] notes;
+
+    private void Start()
+    {
+        foreach(NoteBubble n in notes) {
+            n.playNote += RegisterNote;
+        }
+    }
+
+    public void RegisterNote(NoteBubble nb) {
+        print(nb.name);
+        validateNoteSequence();
+    }
+
+    public void validateNoteSequence() { }
+
+    //IEnumerator resetSequence() {
+
+    //}
+
+    public void playbackSequence() { }
+
+    private void OnDestroy()
+    {
+        foreach (NoteBubble n in notes) {
+            n.playNote -= RegisterNote;
+        }
+    }
+}
